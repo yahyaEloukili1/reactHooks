bf0e9e0f766140edd2704eb3d0b5ef401e622ce8
yahya <yahya.ensas@hotmail.com>
2021-05-26 00:56:35 +0100

    useEffect jsonholder api

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import { useState,useEffect } from "react";
 
 function App() {
   const [resource,setResource] = useState('Users')
+  const [items,setItems] = useState([])
   console.log('render')
   useEffect(()=>{
-    console.log('render type changed')
-  },[resource])
- 
+    fetch(`https://jsonplaceholder.typicode.com/${resource}`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json)
+      setItems(previousItems=>{
+        return json
+      })
+    })
+
+  }, [resource])
+  
+  
    function changeResource(resource){
       setResource(previousResource=> resource)
   }
  
   return (
     <div className="App">
-       <button onClick={()=>{changeResource('Users')}}>Users</button>
-       <button onClick={()=>{changeResource('Posts')}}>Posts</button>
-       <button onClick={()=>{changeResource('Comments')}}>Comments</button>
+       <button onClick={()=>{changeResource('users')}}>Users</button>
+       <button onClick={()=>{changeResource('posts')}}>Posts</button>
+       <button onClick={()=>{changeResource('comments')}}>Comments</button>
        <h3>{resource}</h3>
+      {
+       items.map(item=>{
+         return <p key={item.id}>{JSON.stringify(item)}</p>
+       })
+       }
     </div>
   );
 }
