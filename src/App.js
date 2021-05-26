d8d7e5146e870e132b856b6d86084243bd00166e
yahya <yahya.ensas@hotmail.com>
2021-05-26 01:08:50 +0100

    useEffect windows resize

--- src/App.js
+++ src/App.js
@@ -2,37 +2,22 @@ import './App.css';
 import { useState,useEffect } from "react";
 
 function App() {
-  const [resource,setResource] = useState('Users')
-  const [items,setItems] = useState([])
-  console.log('render')
-  useEffect(()=>{
-    fetch(`https://jsonplaceholder.typicode.com/${resource}`)
-    .then(response => response.json())
-    .then(json => {
-      console.log(json)
-      setItems(previousItems=>{
-        return json
-      })
-    })
+  const [windowWidth,setWindowWidth] = useState(window.innerWidth)
+const handleResize = ()=>{
+  setWindowWidth(window.innerWidth)
+}
+useEffect(()=>{
 
-  }, [resource])
-  
-  
-   function changeResource(resource){
-      setResource(previousResource=> resource)
+  window.addEventListener('resize',handleResize)
+  return ()=>{
+    window.removeEventListener('resize',handleResize)
   }
- 
+})
+  
+  
   return (
     <div className="App">
-       <button onClick={()=>{changeResource('users')}}>Users</button>
-       <button onClick={()=>{changeResource('posts')}}>Posts</button>
-       <button onClick={()=>{changeResource('comments')}}>Comments</button>
-       <h3>{resource}</h3>
-      {
-       items.map(item=>{
-         return <p key={item.id}>{JSON.stringify(item)}</p>
-       })
-       }
+        {windowWidth}
     </div>
   );
 }
